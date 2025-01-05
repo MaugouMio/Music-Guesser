6aa02eee47d76685096d631e19727ec5d799daa4
MaugouMio <maugoumine@gmail.com>
2025-01-05 23:09:49 +0800

    Encapsulate Youtube loading functions

--- Assets/Scripts/YoutubeProcessor.cs
+++ Assets/Scripts/YoutubeProcessor.cs
@@ -1,79 +1,147 @@
 using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 using UnityEngine.Networking;
 using YoutubeExplode;
+using YoutubeExplode.Videos;
 using YoutubeExplode.Videos.Streams;
 using NAudio.Wave;
+using System.Threading.Tasks;
 
 public class YoutubeProcessor : MonoBehaviour
 {
+	public static YoutubeProcessor Instance { get; private set; } = null;
+	public AudioSource AudioPlayer { get; private set; }
+
 	readonly YoutubeClient youtube = new();
 
-	public AudioSource audioPlayer;
+	const int CACHE_SIZE = 100;
+	readonly Dictionary<string, AudioClip> audioCache = new();
+	readonly Queue<string> audioCacheQueue = new();
+	readonly Dictionary<string, Video> infoCache = new();
+	readonly Queue<string> infoCacheQueue = new();
 
-	IEnumerator LoadMusic(string songPath)
+	bool isLoading = false;
+
+	public async Task<Video> GetYoutubeInfo(VideoId videoID)
 	{
-		if (File.Exists(songPath))
+		if (infoCache.TryGetValue(videoID, out var cacheInfo))
+			return cacheInfo;
+
+		isLoading = true;
+
+		// Load youtube audio and save locally ========================================
+
+		var videoInfo = await youtube.Videos.GetAsync(videoID);
+		if (videoInfo == null)
 		{
-			var uwr = UnityWebRequestMultimedia.GetAudioClip("file:///" + songPath, AudioType.MPEG);
-			{
-				((DownloadHandlerAudioClip)uwr.downloadHandler).streamAudio = true;
-				yield return uwr.SendWebRequest();
-
-				if (uwr.result == UnityWebRequest.Result.ConnectionError || uwr.result == UnityWebRequest.Result.ProtocolError)
-				{
-					Debug.LogError(uwr.error);
-					yield break;
-				}
-
-				DownloadHandlerAudioClip dlHandler = (DownloadHandlerAudioClip)uwr.downloadHandler;
-
-				if (dlHandler.isDone)
-				{
-					AudioClip audioClip = dlHandler.audioClip;
-					if (audioClip != null)
-					{
-						audioPlayer.clip = audioClip;
-						audioPlayer.Play();
-						File.Delete("temp.mp3");
-						Debug.Log("Playing song using Audio Source!");
-					}
-					else
-					{
-						Debug.Log("Couldn't find a valid AudioClip :(");
-					}
-				}
-				else
-				{
-					Debug.Log("The download process is not completely finished.");
-				}
-			}
+			Debug.LogWarning($"Can not load youtube video with ID = {videoID}");
+			return null;
 		}
-		else
-		{
-			Debug.Log("Unable to locate converted song file.");
-		}
+
+		infoCache.Add(videoID, videoInfo);
+		infoCacheQueue.Enqueue(videoID);
+		if (infoCacheQueue.Count > CACHE_SIZE)
+			infoCache.Remove(infoCacheQueue.Dequeue());
+
+		Debug.Log($"Successfully loaded {videoID} info!");
+		return videoInfo;
 	}
 
-	async void LoadYoutube(string videoUrl)
+	public async Task<AudioClip> GetYoutubeAudio(VideoId videoID)
 	{
-		var streamManifest = await youtube.Videos.Streams.GetManifestAsync(videoUrl);
-		var streamInfo = streamManifest.GetAudioOnlyStreams().GetWithHighestBitrate();
+		if (isLoading)
+		{
+			Debug.LogError("Already loading a youtube video!");
+			return null;
+		}
 
+		AudioClip audioClip = null;
+		if (audioCache.TryGetValue(videoID, out audioClip))
+			return audioClip;
+
+		isLoading = true;
+
+		// Load youtube audio and save locally ========================================
+
+		var streamManifest = await youtube.Videos.Streams.GetManifestAsync(videoID);
+		if (streamManifest == null)
+		{
+			Debug.LogWarning($"Can not load youtube video stream with ID = {videoID}");
+			isLoading = false;
+			return null;
+		}
+
+		var streamInfo = streamManifest.GetAudioOnlyStreams().GetWithHighestBitrate();
 		await youtube.Videos.Streams.DownloadAsync(streamInfo, "temp.mka");
 
 		var reader = new MediaFoundationReader("temp.mka");
 		MediaFoundationEncoder.EncodeToMp3(reader, "temp.mp3");
-		
 		File.Delete("temp.mka");
-		StartCoroutine(LoadMusic("temp.mp3"));
+
+		// Load locally saved file ========================================
+
+		var uwr = UnityWebRequestMultimedia.GetAudioClip("file:///temp.mp3", AudioType.MPEG);
+		((DownloadHandlerAudioClip)uwr.downloadHandler).streamAudio = true;
+		await uwr.SendWebRequest();
+
+		DownloadHandlerAudioClip dlHandler = (DownloadHandlerAudioClip)uwr.downloadHandler;
+		if (dlHandler.isDone)
+		{
+			audioClip = dlHandler.audioClip;
+			if (audioClip != null)
+			{
+				audioCache.Add(videoID, audioClip);
+				audioCacheQueue.Enqueue(videoID);
+				if (audioCacheQueue.Count > CACHE_SIZE)
+					audioCache.Remove(audioCacheQueue.Dequeue());
+
+				Debug.Log($"Successfully loaded {videoID} audio!");
+			}
+			else
+			{
+				Debug.Log("Couldn't find a valid AudioClip!");
+			}
+		}
+		else
+		{
+			Debug.Log("The download process is not completely finished.");
+		}
+
+		File.Delete("temp.mp3");
+		isLoading = false;
+		return audioClip;
+	}
+
+	void Awake()
+	{
+		if (Instance == null)
+		{
+			AudioPlayer = GetComponent<AudioSource>();
+			if (AudioPlayer == null)
+				AudioPlayer = gameObject.AddComponent<AudioSource>();
+
+			DontDestroyOnLoad(gameObject);
+			Instance = this;
+		}
+		else
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	async void TestPlay()
+	{
+		AudioClip clip = await GetYoutubeAudio("https://www.youtube.com/watch?v=PTUJRM0Ls4s");
+		AudioPlayer.clip = clip;
+		AudioPlayer.Play();
 	}
 
 	// Start is called before the first frame update
 	void Start()
     {
-		LoadYoutube("https://www.youtube.com/watch?v=PTUJRM0Ls4s");
+		TestPlay();
 	}
 
     // Update is called once per frame
